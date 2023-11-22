1682511bdf5ebfd6b282b96ed3fb1f4cfb7cdc60
Jayne Bugani <jaynebugani09@gmail.com>
2023-11-22 17:07:01 +0800

    Add Index and Destroy API for Users

---
+++ app/Http/Controllers/Api/CarouselItemsController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Models\CarouselItems;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CarouselItemsRequest;
+
+class CarouselItemsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return CarouselItems::all();
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CarouselItemsRequest $request)
+    {
+        // Retrieve the validated input data...
+        $validated = $request->validated();
+
+        $carouselItem = CarouselItems::create($validated);
+        
+        /* 
+        $carouselItem = CarouselItems::create([
+        
+           'carousel_name' =>$request->carousel_name,
+        'image_path'    =>$request->image_path,
+        'description'   =>$request->description,
+        'user_id'       =>$request->user_id,
+        ]);*/
+
+        return $carouselItem;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return CarouselItems::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $carouselItem = CarouselItems::findOrFail($id);
+ 
+        $carouselItem->delete();
+
+        return $carouselItem;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\CarouselItemsController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/carousel', [CarouselItemsController::class, 'index']);
+Route::get('/carousel/{id}', [CarouselItemsController::class, 'show']);
+Route::post('/carousel', [CarouselItemsController::class, 'store']);
+Route::put('/carousel/{id}', [CarouselItemsController::class, 'update']);
+Route::delete('/carousel/{id}', [CarouselItemsController::class, 'destroy']);
+
+Route::get('/user', [UserController::class, 'index']);
+Route::get('/user/{id}', [UserController::class, 'show']);
+Route::post('/user', [UserController::class, 'store']);
+Route::delete('/user/{id}', [UserController::class, 'destroy']);
