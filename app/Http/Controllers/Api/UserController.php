275c050470be218916be4dbcd62082c3c90b3906
Jayne Bugani <jaynebugani09@gmail.com>
2023-11-22 17:55:12 +0800

    Add User Request and Show and  Store API

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\User;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\UserRequest;
 
 class UserController extends Controller
 {
+
     /**
      * Display a listing of the resource.
      */
@@ -17,27 +20,11 @@ class UserController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(string $id)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
+        return User::findOrFail($id);
     }
 
     /**
@@ -48,6 +35,21 @@ class UserController extends Controller
         $user = User::findOrFail($id);
 
         $user->delete();
+
+        return $user;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(UserRequest $request)
+    {
+        // Retrieve the validated input data...
+        $validated = $request->validated();
+
+        $validated['password'] = Hash::make($validated['password']);
+
+        $user = User::create($validated);
 
         return $user;
     }
